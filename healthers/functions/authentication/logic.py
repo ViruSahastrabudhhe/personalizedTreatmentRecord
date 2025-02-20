e604ee1853bec75f2d0e49f0acafaec4a84d6349
ViruSahastrabuddhe <s21contactemail@gmail.com>
2025-02-21 04:25:31 +0800

    login class

--- healthers/functions/authentication/logic.py
+++ healthers/functions/authentication/logic.py
@@ -1,2 +1,11 @@
-def calculate(a: int, b: int):
-    return a*b
+from healthers.models import get_db_connection
+import mysql.connector
+from mysql.connector import Error
+
+class Authentication:
+    def __init__(self):
+        self.conn = get_db_connection()
+        self.cursor = self.conn.cursor()
+
+    def signUpAccount(fname, lname, email, password):
+        if self.conn is None:
--- healthers/functions/authentication/routes.py
+++ healthers/functions/authentication/routes.py
@@ -1,12 +1,37 @@
 from . import authentication
 from healthers.models import get_db_connection
 from .logic import calculate
-from flask import render_template
+from flask import render_template, flash, redirect, url_for, request, session
+from werkzeug.security import *
 
 @authentication.route('/')
-def home():
+def welcome():
     conn = get_db_connection()
-    if conn:
-        return "HELLO JAJAJJAJA"
+    if conn is None:
+        flash("NO DB CONNECTION", category='error')
+        return redirect(url_for('authentication.welcome'))
 
-    return render_template('users/homepage/page_login.html')
+    return render_template('users/beforeLogin/page_landing.html', legend="Health record system")
+
+@authentication.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('users/beforeLogin/page_login.html', legend='Sign in to Healthers')
+
+@authentication.route('/signUp', methods=['GET', 'POST'])
+def signUp():
+    if request.method=='POST':
+        fname=request.form['signUpFname']
+        lname=request.form['signUpLname']
+        email=request.form['signUpEmail']
+        password=request.form['signUpPassword']
+
+
+    return render_template('users/beforeLogin/page_signUp.html', legend='Sign up to Healthers')
+
+@authentication.route('/forgotPassword', methods=['GET', 'POST'])
+def forgotPassword():
+    return render_template('users/beforeLogin/page_forgotPassword.html', legend='Forgot password')
+
+@authentication.route('/resetPassword', methods=['GET', 'POST'])
+def resetPassword():
+    return render_template('users/beforeLogin/page_signUp.html', legend='Reset password')
